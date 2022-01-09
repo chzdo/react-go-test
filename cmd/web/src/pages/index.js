ed2932e6d7674660b8bece31248c6cd04d5c9b41
Stanley Nduaguibe <snduaguibe@atbtechsoft.com>
2022-01-09 15:39:11 +0100

    added ind

--- cmd/web/src/pages/index.js
+++ cmd/web/src/pages/index.js
@@ -1,32 +1,30 @@
-import * as React from "react"
-
-
+import * as React from "react";
+import Index from "../components/trial";
 
 // markup
 const IndexPage = () => {
-
-  const [status, setStatus] = React.useState(0)
-  const [message, setMessage] = React.useState("nothing yet")
+  const [status, setStatus] = React.useState(0);
+  const [message, setMessage] = React.useState("nothing yet");
   React.useEffect(() => {
     fetch("/api/test", {
-      method:"GET"
-    }).then(result => result.text()).then(jsonResult => {
-        console.log(jsonResult)
-      jsonResult = JSON.parse(jsonResult)
-      setStatus(jsonResult.status)
-      setMessage(jsonResult.message)
-    }).catch(err=> console.log(err))
-  })
+      method: "GET",
+    })
+      .then((result) => result.text())
+      .then((jsonResult) => {
+        console.log(jsonResult);
+        jsonResult = JSON.parse(jsonResult);
+        setStatus(jsonResult.status);
+        setMessage(jsonResult.message);
+      })
+      .catch((err) => console.log(err));
+  });
   return (
     <>
-      <div>
-        {status}
-      </div>
-      <div>
-        {message}
-      </div>
+      <Index />
+      <div>{status}</div>
+      <div>{message}</div>
     </>
-  )
-}
+  );
+};
 
-export default IndexPage
+export default IndexPage;
